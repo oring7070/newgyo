f004ba32d3142c04265af77c8f0d921aebb947fd
oring7070 <zizon_2710@naver.com>
2025-12-18 16:32:26 +0900

    로컬프로 젝트 원격 저장소에 저장

---
+++ src/main/java/kr/co/newgyo/NewgyoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.newgyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewgyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewgyoApplication.class, args);
+    }
+
+}
