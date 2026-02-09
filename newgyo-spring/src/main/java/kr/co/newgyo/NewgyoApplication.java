804a6ebef80cca36b84df9fdfd53d245780d50d1
YUL554 <hyr0197@gmail.com>
2026-02-09 19:11:28 +0900

    chore : EnableScheduling 위치 변경

--- newgyo-spring/src/main/java/kr/co/newgyo/NewgyoApplication.java
+++ newgyo-spring/src/main/java/kr/co/newgyo/NewgyoApplication.java
@@ -2,9 +2,7 @@ package kr.co.newgyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class NewgyoApplication {
 
--- newgyo-spring/src/main/java/kr/co/newgyo/batch/summary/SummaryScheduler.java
+++ newgyo-spring/src/main/java/kr/co/newgyo/batch/summary/SummaryScheduler.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Component;
  * */
 @Slf4j
 @Component
+@EnableScheduling
 @RequiredArgsConstructor
 public class SummaryScheduler {
     private final PythonApiClient pythonApiClient;
